ff8c7af90518733febb268c9d1aa9acf398c125b
Mathew Polzin <matt.polzin@gmail.com>
2021-12-27 18:11:19 -0800

    tweaks and support for LSP expression evaluation.

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -22,13 +22,16 @@ require('telescope').load_extension('fzf')
 -- 
 local custom_lsp_attach = function(client)
   vim.cmd [[nnoremap <Leader>t <Cmd>lua vim.lsp.buf.hover()<CR>]]
-  vim.cmd [[nnoremap <Leader>e <Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>]]
+  vim.cmd [[nnoremap <Leader>in <Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>]]
   vim.cmd [[nnoremap <c-]> <Cmd>lua vim.lsp.buf.definition()<CR>]]
   vim.cmd [[nnoremap K <Cmd>lua vim.lsp.buf.code_action()<CR>]]
   vim.cmd [[nnoremap <Leader>d <Cmd>lua vim.lsp.buf.signature_help()<CR>]]
 
   -- auto-complete via LSP
   vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- evaluate expression
+  vim.cmd [[nnoremap <Leader>e <Cmd>lua require('idris2.repl').evaluate()<CR>]]
 
   -- open window
   vim.cmd [[nnoremap <Leader>i <Cmd>lua require('idris2.hover').open_split()<CR>]]
@@ -64,21 +67,20 @@ local custom_lsp_attach = function(client)
   vim.cmd [[command Browse lua require('idris2.browse').browse()<CR>]]
 end
 
+local introspect = require('idris2.code_action').introspect_filter
 local filters = require('idris2.code_action').filters
 
-local save_hook = function(err, result, ctx, config)
+local save_hook = function(action)
+  if introspect(action) == filters.MAKE_CASE
+    or introspect(action) == filters.MAKE_WITH then
+      return
+  end
   vim.cmd('sleep 10m')
-  vim.cmd('write')
+  vim.cmd('silent write')
 end
-
-local post_hooks = {
-  [filters.CASE_SPLIT] = save_hook,
-  [filters.MAKE_LEMMA] = save_hook,
-  [filters.ADD_CLAUSE] = save_hook
-}
 
 require('idris2').setup({
   server = {on_attach = custom_lsp_attach},
-  post_hooks = post_hooks
+  code_action_post_hook = save_hook
 })
 
