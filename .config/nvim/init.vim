e85fd95382c7217ea25bd94dfb7f357a3ba376e4
Mathew Polzin <matt.polzin@gmail.com>
2022-01-09 15:47:07 -0800

    Add gitsigns plugin, support format options to LSP format call, support split jump to definition.

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -4,18 +4,25 @@ source ~/.vimrc
 
 call plug#begin()
 
+" Util
+Plug 'nvim-lua/plenary.nvim'
+Plug 'MunifTanjim/nui.nvim'
+
+" Tab completion
 Plug 'ervandew/supertab'
 
-Plug 'nvim-lua/plenary.nvim'
+" Browsing/Finding
 Plug 'BurntSushi/ripgrep'
 Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
 Plug 'nvim-telescope/telescope.nvim'
 
+" LSP
 Plug 'neovim/nvim-lspconfig'
-Plug 'MunifTanjim/nui.nvim'
 Plug 'ShinKage/idris2-nvim'
-
 Plug 'elixir-editors/vim-elixir'
+
+" Git
+Plug 'lewis6991/gitsigns.nvim'
 
 call plug#end()
 
--- .config/nvim/lua/lsp/common.lua
+++ .config/nvim/lua/lsp/common.lua
@@ -4,11 +4,15 @@
 --
 local M = {}
 
-function M.setup()
+-- options = {
+--   formatting_options = {} -- sent to LSP along with textDocument/format command.
+-- }
+function M.setup(options)
 
   vim.cmd [[nnoremap <Leader>t <Cmd>lua vim.lsp.buf.hover()<CR>]]
   vim.cmd [[nnoremap <Leader>in <Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>]]
   vim.cmd [[nnoremap <c-]> <Cmd>lua vim.lsp.buf.definition()<CR>]]
+  vim.cmd [[nnoremap <c-w>] :vsplit <bar> lua vim.lsp.buf.definition()<CR>]]
   vim.cmd [[nnoremap K <Cmd>lua vim.lsp.buf.code_action()<CR>]]
   vim.cmd [[nnoremap <Leader>d <Cmd>lua vim.lsp.buf.signature_help()<CR>]]
   vim.cmd [[nnoremap [d <cmd>lua vim.diagnostic.goto_prev()<CR>]]
@@ -18,7 +22,14 @@ function M.setup()
   vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- format current buffer
-  vim.cmd [[command! Format lua vim.lsp.buf.formatting()<CR>]]
+  function M.format()
+    if options and options.formatting_options then
+      vim.lsp.buf.formatting({options = options.formatting_options})
+    else
+      vim.lsp.buf.formatting()
+    end
+  end
+  vim.cmd [[command! Format lua require('lsp.common').format()<CR>]]
 end
 
 return M
