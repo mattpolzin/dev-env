479bb9aa626562ebaf851a76970faa148e83927e
= <=>
2022-03-04 20:55:23 -0800

    give myself mac-os-like Cmd+q to quit, move reload to Cmd+Shift+r

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,20 +1,27 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
 
 builtinDisplay = "eDP"
+
+myModMask = mod4Mask
 
 main = xmonad =<< xmobar myConfig
 
 myConfig = def
   { terminal = "kitty"
-  , modMask  = mod4Mask
+  , modMask  = myModMask
   , layoutHook  = myLayoutHook
   , manageHook  = myManageHook
   , startupHook = myStartupHook
   }
-
+  `additionalKeys` myKeys
+  
+myKeys = [ ((myModMask, xK_q), kill)
+         , ((myModMask .|. shiftMask, xK_r), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
+         ]
 
 myManageHook = manageDocks <+> XMonad.manageHook defaultConfig
 myLayoutHook = avoidStruts $ XMonad.layoutHook defaultConfig
