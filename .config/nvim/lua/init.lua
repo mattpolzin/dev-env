21b0cf019a90a3e0f2fb2292cb1e6e56283786fa
Mathew Polzin <matt.polzin@gmail.com>
2023-01-21 18:20:52 -0600

    use top line for context rather than cursor.

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -205,7 +205,9 @@ require'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 }
-require('treesitter-context').setup {}
+require('treesitter-context').setup {
+  mode = 'topline'
+}
 
 
 --
