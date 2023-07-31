1189592444509c0488e0a4514d64fa67bba18a47
Mathew Polzin <matt.polzin@gmail.com>
2023-07-31 11:57:17 -0500

    add swift lsp module

---
+++ .config/nvim/lua/lsp/swift.lua
@@ -0,0 +1,20 @@
+
+--
+-- Swift LSP
+--
+local common = require('lsp.common')
+
+local function custom_lsp_attach(client)
+  common.setup()
+end
+
+local M = {}
+
+function M.setup()
+  require('lspconfig').sourcekit.setup({
+    on_attach = custom_lsp_attach,
+    capabilities = common.capabilities
+  })
+end
+
+return M
