7be0f54d460591414bf3c2e16ea25ae199aa94bf
Matt Polzin <mpolzin@workwithopal.com>
2023-08-23 08:35:05 -0500

    switch from ruby-lsp to solargraph. disable broken tab features of autolist.

--- .config/nvim/lua/lsp/ruby.lua
+++ .config/nvim/lua/lsp/ruby.lua
@@ -5,34 +5,15 @@
 local common = require('lsp.common')
 
 local function custom_lsp_attach(client)
-  common.setup({ skip_go_to_def = true })
+  common.setup()
 end
-
--- local path_to_rubyls = vim.fn.expand("bundle exec ruby-lsp")
 
 local M = {}
 
 function M.setup()
-  require('lspconfig').ruby_ls.setup({
-    cmd = {'bundle', 'exec', 'ruby-lsp'},
+  require('lspconfig').solargraph.setup({
     on_attach = custom_lsp_attach,
     capabilities = common.capabilities,
-    init_options = {
-      enabledFeatures = {
-        'codeActions',
-        'diagnostics',
-        'documentHighlights',
-        'documentSymbols',
-        'formatting',
-        'hover',
-        'inlayHint',
-        'foldingRanges',
-        'selectionRanges',
-        'semanticHighlighting',
-        'onTypeFormatting',
-        'codeLens',
-      },
-    },
   })
 end
 
