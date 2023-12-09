bb549d9ba17505fe316be47020c5e94339dda75e
Mathew Polzin <matt.polzin@gmail.com>
2023-12-08 23:12:37 -0600

    Don’t set mix.package to what is its default value anyway.

--- work-configuration.nix
+++ work-configuration.nix
@@ -183,7 +183,6 @@ in
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  nix.package = pkgs.nix;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;
