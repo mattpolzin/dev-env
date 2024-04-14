0f090312c02d8d84a10573459e2269f90202b6c9
Mathew Polzin <matt.polzin@gmail.com>
2024-04-14 17:06:47 -0500

    Build custom iosevka and start using it in terminals

--- nix/modules/common/system.nix
+++ nix/modules/common/system.nix
@@ -109,6 +109,43 @@ in {
     fonts = [
       (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];})
       pkgs-edge.pixel-code
+      (pkgs-edge.iosevka.override {
+        privateBuildPlan = {
+          family = "Iosevka Custom";
+          spacing = "term";
+          serifs = "sans";
+          noCvSs = true;
+          exportGlyphNames = true;
+
+          # JetBrains Mono:
+          # variants.inherits = "ss14";
+
+          ligations = {
+            inherits = "dlig";
+            enables = [ "slasheq" ];
+          };
+
+          widths = {
+            Normal = {
+              shape = 600;
+              menu = 5;
+              css = "normal";
+            };
+          };
+
+          weights = let weight = n: { shape = n; menu = n; css = n; }; in {
+            Thin = weight 100;
+            ExtraLight = weight 200;
+            Light = weight 300;
+            Regular = weight 400;
+            Medium = weight 500;
+            SemiBold = weight 600;
+            Bold = weight 700;
+            ExtraBold = weight 800;
+          };
+        };
+        set = "Custom";
+      })
     ];
   };
 
