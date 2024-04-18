b95eb88bd10dad9383aa67e646ffec8f8d39375c
Mathew Polzin <matt.polzin@gmail.com>
2024-04-17 23:20:26 -0500

    simplify diff script and remove troubleshooting echos

--- nix-diff.sh
+++ nix-diff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-eval LAST_TWO_VERSIONS=("$(ls /nix/var/nix/profiles | cut -c 8-100 | sort -h -r | head -2 | sort -h)")
+eval LAST_TWO_VERSIONS=("$(ls /nix/var/nix/profiles | cut -c 8-100 | sort -h | tail -2)")
 
 if [ ! -z "$1" ]; then
   LOWER_BOUND="/nix/var/nix/profiles/system-$1-link"
@@ -13,8 +13,5 @@ if [ ! -z "$2" ]; then
 else
   UPPER_BOUND="/nix/var/nix/profiles/system-${LAST_TWO_VERSIONS[1]}"
 fi
-
-echo $LOWER_BOUND
-echo $UPPER_BOUND
 
 nix run nixpkgs#nvd -- diff "${LOWER_BOUND}" "${UPPER_BOUND}"
