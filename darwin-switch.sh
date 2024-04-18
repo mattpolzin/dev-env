443e6bd7cbdd02b6f8b520b4a3469d3bb45083ee
Mathew Polzin <mpolzin@workwithopal.com>
2024-04-18 11:11:38 -0500

    Add script to automate switching and printing system diff

---
+++ darwin-switch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eu -o pipefail
+
+eval LAST_VERSION_BEFORE=("$(ls /nix/var/nix/profiles | cut -c 8-100 | sort -h | tail -1)")
+
+darwin-rebuild switch --flake . --keep-going
+
+eval LAST_VERSION_AFTER=("$(ls /nix/var/nix/profiles | cut -c 8-100 | sort -h | tail -1)")
+
+if [ "${LAST_VERSION_BEFORE}" = "${LAST_VERSION_AFTER}" ]; then
+  echo ''
+  echo 'No system changes needed.'
+else
+  ./nix-diff.sh
+fi
