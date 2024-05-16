3e4010a8f05151ee8f964175e7038ee5b07cf93a
Mathew Polzin <matt.polzin@gmail.com>
2024-05-15 22:15:15 -0500

    regreet is way better and faster

--- nix/modules/common/river-system.nix
+++ nix/modules/common/river-system.nix
@@ -1,33 +1,21 @@
 { pkgs, config, ... }:
-let
-  greetConfig = pkgs.writeText "greetd-sway-config" ''
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
-
-    bindsym Mod4+Shift+e exec swaynag \
-      -t warning \
-      -m 'What do you want to do?' \
-      -b 'Poweroff' 'systemctl poweroff' \
-      -b 'Reboot' 'systemctl reboot'
-
-    include /etc/sway/config.d/*
-  '';
-in
 {
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.sway}/bin/sway --config ${greetConfig}";
         user = "${config.users.primary}";
       };
     };
   };
-
-  environment.etc."greetd/environments".text = ''
-    river
-    sway
-    zsh
-  '';
+  programs.regreet = {
+    enable = true;
+    settings = {
+      GTK = {
+        application_prefer_dark_theme = true;
+      };
+    };
+  };
 
   programs.river = {
     enable = true;
