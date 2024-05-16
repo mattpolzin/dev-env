415795c31332cac323e6373eecfb74dcd76f7c78
Mathew Polzin <matt.polzin@gmail.com>
2024-05-15 20:08:30 -0500

    a technically working but fairly clunky login experience

--- nix/modules/common/river-system.nix
+++ nix/modules/common/river-system.nix
@@ -1,12 +1,39 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  greetConfig = pkgs.writeText "greetd-sway-config" ''
+    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+
+    bindsym Mod4+Shift+e exec swaynag \
+      -t warning \
+      -m 'What do you want to do?' \
+      -b 'Poweroff' 'systemctl poweroff' \
+      -b 'Reboot' 'systemctl reboot'
+
+    include /etc/sway/config.d/*
+  '';
+in
 {
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.sway}/bin/sway --config ${greetConfig}";
+        user = "${config.users.primary}";
+      };
+    };
+  };
+
+  environment.etc."greetd/environments".text = ''
+    river
+    sway
+    zsh
+  '';
+
   programs.river = {
     enable = true;
     extraPackages = with pkgs; [
-#      swaymonad
       wl-clipboard
       tofi
-#      wlgreet
     ];
   };
   programs.waybar.enable = true;
