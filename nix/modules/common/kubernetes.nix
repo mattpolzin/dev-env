9676a8b5e886fa8111b1203f48058ebc733f2590
Mathew Polzin <matt.polzin@gmail.com>
2024-05-26 18:41:21 -0500

    trying to get a simple kubernetes config up and running

--- nix/modules/common/kubernetes.nix
+++ nix/modules/common/kubernetes.nix
@@ -1,11 +1,5 @@
 { lib, config, pkgs, ... }:
 let
-  # When using easyCerts=true the IP Address must resolve to the master on creation.
- # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
-  kubeMasterIP = "10.1.1.2";
-  kubeMasterHostname = "api.kube";
-  kubeMasterAPIServerPort = 6443;
-
   cfg = config.personal.kubernetes;
 in
 {
@@ -25,9 +19,6 @@ in
       }
     ];
 
-    # resolve master hostname
-    networking.extraHosts = lib.mkIf cfg.enable "${kubeMasterIP} ${kubeMasterHostname}";
-
     # packages for administration tasks
     environment.systemPackages = lib.mkIf cfg.enable (with pkgs; [
       kubectl
@@ -36,16 +27,11 @@ in
 
     services.kubernetes = lib.mkIf cfg.enable {
       roles = ["master" "node"];
-      masterAddress = kubeMasterHostname;
-      apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+      masterAddress = "localhost";
       easyCerts = true;
-      apiserver = {
-        securePort = kubeMasterAPIServerPort;
-        advertiseAddress = kubeMasterIP;
-      };
 
       # use coredns
-      addons.dns.enable = true;
+#      addons.dns.enable = true;
 
       # needed if you use swap
       kubelet.extraOpts = "--fail-swap-on=false";
