715f7e736628fa40cadc0f4596e9d23bb53b96e4
Mathew Polzin <mpolzin@workwithopal.com>
2026-07-15 10:46:33 -0500

    don't manage Font Book with yabai

--- nix/modules/shared/darwin-system.nix
+++ nix/modules/shared/darwin-system.nix
@@ -85,6 +85,7 @@ in
       yabai -m rule --add app='Zoom Workplace' manage=off
       yabai -m rule --add app='Zoom' manage=off
       yabai -m rule --add app='zoom.us' manage=off
+      yabai -m rule --add title='Font Book' manage=off
       yabai -m rule --add title='Trash' manage=off
       yabai -m rule --add title='Web Inspector' manage=off
     '';
