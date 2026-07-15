356e7c0a12c2449f515ab9b87363661c19e04aaa
Mathew Polzin <mpolzin@workwithopal.com>
2026-07-15 11:49:25 -0500

    support another way of patching nixpkgs-edge. add patch that gets iosevka font building on macos again

--- nix/modules/shared/nixpkgs-edge.nix
+++ nix/modules/shared/nixpkgs-edge.nix
@@ -3,14 +3,17 @@ let
   extraNixpkgsEdgeOverlays = [ ];
 in
 { nixpkgs-edge, 
-  patches ? [],
+  patches ? [../../patches/iosevka-fix-2026-07-07.patch],
+  patchCommits ? [], # [{ fork = "mattpolzin"; commit = "97dd352dc415e846fb278b773ff476bb38a80afb"; hash = ""; }]
   overlays ? [],
 system }:
 let
-  nixpkgsEdgePatched = (import nixpkgs-edge { inherit system; }).applyPatches {
+  nixpkgs = import nixpkgs-edge { inherit system; };
+  commitPatches = map ({fork ? "nixos", commit, hash}: nixpkgs.fetchpatch2 {url = "https://github.com/${fork}/nixpkgs/commit/${commit}.patch"; inherit hash;}) patchCommits;
+  nixpkgsEdgePatched = nixpkgs.applyPatches {
     name = "nixpkgs-patched";
     src = nixpkgs-edge;
-    patches = patches ++ extraNixpkgsEdgePatches;
+    patches = patches ++ commitPatches ++ extraNixpkgsEdgePatches;
   };
 in
 { pkgs, ... }:
