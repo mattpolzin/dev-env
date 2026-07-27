38ce19b9c0084dc2eee63a02febb6be1965b65e9
Mathew Polzin <mpolzin@workwithopal.com>
2026-07-27 17:03:44 -0500

    fix LspLog command breaking whenever specific LSP config fails. add ruby-lsp config

--- .config/nvim/lua/lsp/ruby.lua
+++ .config/nvim/lua/lsp/ruby.lua
@@ -10,12 +10,26 @@ end
 
 local M = {}
 
-function M.setup()
+local function solargraph()
   vim.lsp.config("solargraph", {
     on_attach = custom_lsp_attach,
     capabilities = common.capabilities,
   })
   vim.lsp.enable("solargraph")
+end
+
+local path_to_ruby_lsp = vim.fn.expand("ruby-lsp")
+local function ruby_lsp()
+  vim.lsp.config("ruby-lsp", {
+    on_attach = custom_lsp_attach,
+    capabilities = common.capabilities,
+    cmd = {path_to_ruby_lsp, "--use-launcher"},
+  })
+  vim.lsp.enable("ruby-lsp")
+end
+
+function M.setup()
+  ruby_lsp()
 end
 
 return M
