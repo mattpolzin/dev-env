e4d495cc70404cbf82843caddfaedb2a0c08cdab
Mathew Polzin <mpolzin@workwithopal.com>
2026-07-29 14:26:23 -0500

    update flake inputs, align naming of slack Mac App Store brew install

--- nix/modules/home/darwin-system.nix
+++ nix/modules/home/darwin-system.nix
@@ -29,7 +29,7 @@
   homebrew.masApps = {
     "Pixelmator Pro" = 1289583905;
     "Affinity Publisher" = 881418622;
-    "Slack for Desktop" = 803453959;
+    Slack = 803453959;
   };
 
   networking.wakeOnLan.enable = true;
